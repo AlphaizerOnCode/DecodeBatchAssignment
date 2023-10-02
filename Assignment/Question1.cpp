0e6a59da197487a95f8ed1a75d4d07ffd263ff53
AlphaizerOnCode <146561685+AlphaizerOnCode@users.noreply.github.com>
2023-10-02 19:53:38 +0530

    Add files via upload

---
+++ Assignment/Question1.cpp
@@ -0,0 +1,12 @@
+//Write a program to 
+//count the minimum number of notes in a given
+//amount using the switch statement.
+
+#include<iostream>
+using namespace std;
+int main(){
+switch(x){
+    case 500:
+        cout<<"Notes of “500” = "
+}
+}
